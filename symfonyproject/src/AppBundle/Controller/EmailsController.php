9775a42cf96763c20e31c5abf44a1a5c0b8fa070
slimbc <slim.chaaben2@gmail.com>
2019-01-03 17:52:07 +0100

    L'ajout du mail a la bd

--- symfonyproject/src/AppBundle/Controller/EmailsController.php
+++ symfonyproject/src/AppBundle/Controller/EmailsController.php
@@ -3,11 +3,13 @@
 namespace AppBundle\Controller;
 
 use AppBundle\Entity\email;
+use AppBundle\Entity\Event;
+use AppBundle\Entity\User;
+use Psr\Log\LoggerInterface;
 use AppBundle\Form\emailType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\Request;
-
 class EmailsController extends Controller
 {
 
@@ -28,11 +30,8 @@ class EmailsController extends Controller
     public function addmail(Request $req,\Swift_Mailer $mailer)
     {
 
-        $message = (new \Swift_Message('Hello Email'))
-            ->setFrom('slim.chaaben2@gmail.com')
-            ->setTo('slim.chaaben2@gmail.com')
-            ->setBody('slim test');
-        $mailer->send($message);
+
+
 
         $e = new email();
         $form  =$this->createForm(emailType::class,$e);
@@ -45,6 +44,19 @@ class EmailsController extends Controller
             $e->setDate(new \DateTime('now'));
             $e->setCreater($user);
             $em->persist($e);
+
+
+            $users= array();
+            $u = null;
+            $users=$this->getDoctrine()->getRepository(User::class)->findAll();
+            foreach($users as $u)
+            {
+                $message = (new \Swift_Message($e->getObject()))
+                    ->setFrom('nategprojet@gmail.com')
+                    ->setTo($u->getEmail())
+                    ->setBody($e->getContent());
+                $mailer->send($message);
+            }
             $em->flush();
         }
 
@@ -53,5 +65,21 @@ class EmailsController extends Controller
             'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
         ]);
     }
+
+
+
+    /**
+     * @Route("/mail/show",name="show_mail")
+     */
+    public function showAction(LoggerInterface $logger)
+    {       $mails= array();
+            $mails=$this->getDoctrine()->getRepository(email::class)
+                ->findAll();
+            return $this->render('@App/Emails/Show.html.twig', array(
+            "mails"=>$mails
+        ));
+    }
+
+
 
 }
